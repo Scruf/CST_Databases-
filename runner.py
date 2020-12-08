d781790aae151acaa410b472f4550ad33cfe42f3
Scruf <ekozitski@hotmail.com>
2020-12-08 19:13:59 +0000

    iAdded wrtiing to different csv

--- runner.py
+++ runner.py
@@ -2,6 +2,7 @@ from random_data_generator import *
 from random import randrange
 
 accounts = []
+clients = []
 for i in range(0, 10):
     account = AccountFactory().dict()
     account_id = account['account_id']
@@ -47,25 +48,54 @@ for i in range(0, 10):
                     'disp_id': disp['disp_id']    
                 })
                 disp['cards'].append(card)
-            account['disps'].append(disp)
+        client.update({
+            'disps': disp 
+        })
+        clients.append(client)
     accounts.append(account)
 
-account = accounts[0]
 exclude_fields = {'loans', 'transactions', 'orders', 'disps'}
-
+import pdb;pdb.set_trace()
 columns = []
 values = []
 from datetime import datetime
-import csv
+child_dicts = {
+        'loans': [],
+        'transactions':[],
+        'orders': []
+}
 headers = []
-for field in account:
-    if field not in exclude_fields:
-        columns.append(field)
-        values.append(str(account[field]))
-rows = dict(zip(columns, values))
-with open('writer.csv', 'w') as w:
-    w.write(",".join(columns)+"\n")
-    w.write(",".join(values)+"\n")
+with open('account.csv', 'w') as writer:
+    header = []
+    for account in accounts:
+        for field in account:
+            if field not in exclude_fields:
+                columns.append(field)
+                values.append(str(account[field]))
+            else:
+                if field in child_dicts:
+                    child_dicts[field].extend(account[field])
+        if not header:
+            header = columns
+            writer.write(",".join(header)+"\n")
+        writer.write(",".join(values)+"\n")
+        values = []
+for child in child_dicts:
+    with open(f"{child}.csv", "w") as writer:
+        is_header = False
+        for data in child_dicts[child]:
+            if not is_header:
+                is_header = True
+                writer.write(",".join(list(data.keys()))+"\n")
+            writer.write(",".join(list(map(lambda x: str(x), list(data.values()))))+"\n")
 
+with open('client.csv', 'w') as writer:
+    is_client_header = False
+    for client in clients:
+        disps = client.pop('disps')
+        if not is_client_header:
+            writer.write(",".join(list(client.keys()))+"\n")
+            is_client_header = True
+        
 
         
